26a8057b4a57ccfa02f79d5277220a2f73d4a51d
Dmitry Pavlov <dpavlov@griddynamics.com>
2023-03-14 12:26:40 -0600

    query update

--- example-bfloat16.py
+++ example-bfloat16.py
@@ -59,31 +59,7 @@ def main(
     generator = load(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size)
 
     prompts = [
-        ##### For these prompts, the expected answer is the natural continuation of the prompt #####
-
-        "I believe the meaning of life is",
-        # "Simply put, the theory of relativity states that ",
-        # "Building a website can be done in 10 simple steps:\n",
-
-        ##### Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api #####
-
-        #         """Tweet: "I hate it when my phone battery dies."
-        # Sentiment: Negative
-        # ###
-        # Tweet: "My day has been 👍"
-        # Sentiment: Positive
-        # ###
-        # Tweet: "This is the link to the article"
-        # Sentiment: Neutral
-        # ###
-        # Tweet: "This new music video was incredibile"
-        # Sentiment:""",
-
-        #         """Translate English to French:
-        # sea otter => loutre de mer
-        # peppermint => menthe poivrée
-        # plush girafe => girafe peluche
-        # cheese =>""",
+        'how to get request embedding from you?',
     ]
 
     results = generator.generate(
