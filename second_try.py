2c9f60ff15f86c77377f0391ff6babd66d1fc3e2
Dmitry Pavlov <dpavlov@griddynamics.com>
2023-03-17 13:16:03 -0600

    second try

--- second_try.py
+++ second_try.py
@@ -1,12 +1,19 @@
 from transformers import pipeline
+import pickle
 
 clf = pipeline(
     task = 'feature-extraction',
     model = 'deerslab/llama-7b-embeddings')#'shalomma/llama-7b-embeddings')
 
 text = ["McDonald's is a famous fast food chain.",
-        'Anna loves fast food.',]
+        'Anna loves fast food.',
+        'she likes hamburgers!',
+        'Where is Mona Lisa located?',
+        'the famous davinci painting is in paris']
 
 result = clf(text)
 
-print(result)
+with open(f'emb.pkl', 'wb') as f:
+    pickle.dump(result, f)
+
+#print(result)
