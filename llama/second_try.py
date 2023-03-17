626d0d921c0799e81d4a85dcdb514ab374c468b3
Dmitry Pavlov <dpavlov@griddynamics.com>
2023-03-17 12:57:09 -0600

    second try

---
+++ llama/second_try.py
@@ -0,0 +1,12 @@
+from transformers import pipeline
+
+clf = pipeline(
+    task = 'feature-extraction',
+    model = 'deerslab/llama-7b-embeddings')#'shalomma/llama-7b-embeddings')
+
+text = ["McDonald's is a famous fast food chain.",
+        'Anna loves fast food.',]
+
+clf(text)
+
+print(clf)
