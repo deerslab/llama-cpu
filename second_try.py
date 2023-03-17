dcf4667215651a8a9a213f5b61ed3535365a0f69
Dmitry Pavlov <dpavlov@griddynamics.com>
2023-03-17 14:51:14 -0600

    languages

--- second_try.py
+++ second_try.py
@@ -5,15 +5,16 @@ clf = pipeline(
     task = 'feature-extraction',
     model = 'deerslab/llama-7b-embeddings')#'shalomma/llama-7b-embeddings')
 
-text = ["McDonald's is a famous fast food chain.",
-        'Anna loves fast food.',
-        'she likes hamburgers!',
+text = ['Anna loves fast food.',
+        'Анна любит фаст-фуд.'
+        'Anna ama la comida rápida.',
         'Where is Mona Lisa located?',
-        'the famous davinci painting is in paris']
+        'Где находится Мона Лиза?',
+        '¿Dónde se encuentra la Mona Lisa?']
 
 result = clf(text)
 
-with open(f'emb.pkl', 'wb') as f:
+with open(f'emb_lang.pkl', 'wb') as f:
     pickle.dump(result, f)
 
 #print(result)
