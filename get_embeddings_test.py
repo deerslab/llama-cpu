338f3b84a5f5e7a9046e85f153208f59ce527c98
Dmitry Pavlov <dpavlov@griddynamics.com>
2023-03-21 12:16:14 -0600

    pooling over embeddings

--- get_embeddings_test.py
+++ get_embeddings_test.py
@@ -4,11 +4,14 @@ from transformers import LlamaForCausalLM, LlamaTokenizer
 
 import pickle
 
-transformer_name = 'decapoda-research/llama-7b-hf'
+def get_model():
 
-tokenizer = LlamaTokenizer.from_pretrained(transformer_name, use_fast=True, pad_token='<pad>')
+    transformer_name = 'decapoda-research/llama-7b-hf'
+    tokenizer = LlamaTokenizer.from_pretrained(transformer_name, use_fast=True, pad_token='<pad>')
+    model = LlamaForCausalLM.from_pretrained(transformer_name)
+    return model, tokenizer
 
-model = LlamaForCausalLM.from_pretrained(transformer_name)
+model, tokenizer = get_model()
 
 sentences = ['Anna loves fast food.',
         'Анна любит фаст-фуд.',
@@ -16,17 +19,26 @@ sentences = ['Anna loves fast food.',
         'Where is Mona Lisa located?',
         'Где находится Мона Лиза?',
         '¿Dónde se encuentra la Mona Lisa?']
-tokenizer_result = tokenizer(sentences, return_attention_mask=True, return_tensors='pt', padding=True)
 
-input_ids = tokenizer_result.input_ids
-attention_mask = tokenizer_result.attention_mask
+def feed_sentence(sentences):
+    tokenizer_result = tokenizer(sentences, return_attention_mask=True, return_tensors='pt', padding=True)
+    input_ids = tokenizer_result.input_ids
+    attention_mask = tokenizer_result.attention_mask
 
-model_result = model(input_ids, attention_mask=attention_mask, return_dict=True)
+    model_result = model(input_ids, attention_mask=attention_mask, return_dict=True)
 
-token_embeddings = model_result.last_hidden_state
+    token_embeddings = model_result.last_hidden_state
 
-pooled = torch.max((token_embeddings * attention_mask.unsqueeze(-1)), axis=1)
-mean_pooled = token_embeddings.sum(axis=1) / attention_mask.sum(axis=-1).unsqueeze(-1)
+    pooled = torch.max((token_embeddings * attention_mask.unsqueeze(-1)), axis=1)
+    mean_pooled = token_embeddings.sum(axis=1) / attention_mask.sum(axis=-1).unsqueeze(-1)
+
+    return pooled,mean_pooled
+
+pooled, mean_pooled = [], []
+for s in sentences:
+    max_p, mean_p = feed_sentence([s])
+    pooled.append(max_p)
+    mean_pooled.append(mean_p)
 
 with open(f'emb_maxpool.pkl', 'wb') as f:
     pickle.dump(pooled, f)
